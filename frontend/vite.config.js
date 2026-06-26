deb80788b94fa82040f41e1a99cdcf72811ca002
Megha Singh <meghashakya235@gmail.com>
2026-06-26 21:09:49 +0530

    Remove Flowbite Vite plugin

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,13 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import flowbiteReact from "flowbite-react/plugin/vite";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
-    flowbiteReact(),
   ],
   server: {
     port: 3000,
